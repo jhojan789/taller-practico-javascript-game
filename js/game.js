f554528d5f6a3ac2531c952fbce0071e7893900a
Jhojan Tobón <jhojantobonm@gmail.com>
2023-05-24 17:07:46 -0500

    Testing the canvas' methods

--- js/game.js
+++ js/game.js
@@ -1,1 +1,19 @@
-console.log(maps);
+const canvas = document.querySelector('.game');
+const game = canvas.getContext('2d');
+
+
+window.addEventListener('load', startGame);
+
+function startGame(){
+
+  // game.fillRect(0,0,100,100);
+  // game.clearRect(10,10,50,50);
+  
+  game.font = '25px Sanserif';
+  game.fillStyle = 'purple';
+  game.textAlign = 'left';
+  game.fillText('Jhojan', 0 , 25);
+
+
+}
+
