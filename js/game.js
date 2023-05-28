e5721d6c2c00f6b2cd3681a24822a5b3263c6b58
Jhojan Tobón <jhojantobonm@gmail.com>
2023-05-28 17:19:38 -0500

    Canvas size and its elements

--- js/game.js
+++ js/game.js
@@ -5,14 +5,36 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', startGame);
 
 function startGame(){
-
-  // game.fillRect(0,0,100,100);
-  // game.clearRect(10,10,50,50);
   
-  game.font = '25px Sanserif';
-  game.fillStyle = 'purple';
-  game.textAlign = 'left';
-  game.fillText('Jhojan', 0 , 25);
+  let canvasSize;
+
+  if(window.innerHeight > window.innerWidth){
+    canvasSize = window.innerWidth * 0.8;
+
+  }else{
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute('width',canvasSize);
+  canvas.setAttribute('height',canvasSize);
+
+  const elementSize = canvasSize / 10;
+
+  game.font = elementSize + 'px Verdana';
+  
+  game.textAlign = 'end'
+  for (let i = 1; i <= 10; i++) {
+    
+    game.fillText(emojis['X'],elementSize,elementSize * i);
+    
+  }
+ 
+  
+  
+  console.log(window.innerHeight, window.innerWidth);
+  console.log(canvasSize);
+  console.log(elementSize);
+
 
 
 }
